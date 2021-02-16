5341425c438fcb4e4990f051614c4b9671245c27
ouzhrm <ouhzrm@outlook.com>
2021-02-16 16:33:54 +0800

    add

---
+++ src/com/ouhzrm/算法/树_leetcode热门前100道题/TreeNode.java
@@ -0,0 +1,14 @@
+package com.ouhzrm.算法.树_leetcode热门前100道题;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
