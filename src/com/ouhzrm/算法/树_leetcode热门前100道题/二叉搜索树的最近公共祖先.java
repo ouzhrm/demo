4ea0884b1b16246b25b53081485f29c9ce5eeae5
ouzhrm <ouhzrm@outlook.com>
2021-03-07 15:28:10 +0800

    add

---
+++ src/com/ouhzrm/算法/树_leetcode热门前100道题/二叉搜索树的最近公共祖先.java
@@ -0,0 +1,21 @@
+package com.ouhzrm.算法.树_leetcode热门前100道题;
+
+/**
+ * @description:
+ * @author(itcode): zhouyh3
+ * @create: 2021-03-07 15:21
+ **/
+public class 二叉搜索树的最近公共祖先 {
+    /**
+     https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-zui-jin-gong-gong-zu-xian-lcof/
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root.val < p.val && root.val < q.val){
+            return lowestCommonAncestor(root.right,p,q);
+        }
+        if(root.val > p.val && root.val > q.val){
+            return lowestCommonAncestor(root.left,p,q);
+        }
+        return root;
+    }
+}
---
+++ src/com/ouhzrm/算法/树_leetcode热门前100道题/二叉树的镜像.java
@@ -0,0 +1,21 @@
+package com.ouhzrm.算法.树_leetcode热门前100道题;
+
+/**
+ * @description:
+ * @author(itcode): zhouyh3
+ * @create: 2021-03-07 14:13
+ **/
+public class 二叉树的镜像 {
+    /**
+     * https://leetcode-cn.com/problems/er-cha-shu-de-jing-xiang-lcof/
+     */
+    public TreeNode mirrorTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode temp = root.right;
+        root.right = mirrorTree(root.left);
+        root.left = mirrorTree(temp);
+        return root;
+    }
+}
