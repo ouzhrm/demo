836c028507850389bf8c2a56d9f9e86efa1004e7
ouzhrm <ouhzrm@outlook.com>
2021-03-13 19:52:27 +0800

    add

---
+++ src/com/ouhzrm/算法/链表/ListNode.java
@@ -0,0 +1,42 @@
+package com.ouhzrm.算法.链表;
+
+/**
+ * @description:
+ * @author(itcode): zhouyh3
+ * @create: 2021-03-13 18:35
+ **/
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public static ListNode get(int[] s) {
+        ListNode root = new ListNode(s[0]);//生成链表的根节点，并将数组的第一个元素的值赋给链表的根节点
+        ListNode other = root;//生成另一个节点，并让other指向root节点，other在此作为一个临时变量，相当于指针
+        for (int i = 1; i < s.length; i++) {//由于已给root赋值，所以i从1开始
+            ListNode temp = new ListNode(s[i]);//每循环一次生成一个新的节点,并给当前节点赋值
+            other.next = temp;//将other的下一个节点指向生成的新的节点
+            other = temp;//将other指向最后一个节点(other的下一个节点)  other=other.getNext();
+        }
+        return root;
+    }
+
+    public static void print(ListNode node){
+        if(node == null) return;
+        System.out.println(node.val);
+        print(node.next);
+    }
+}
+
+
---
+++ src/com/ouhzrm/算法/链表/移除链表元素.java
@@ -0,0 +1,33 @@
+package com.ouhzrm.算法.链表;
+
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ * @description:
+ * @author(itcode): zhouyh3
+ * @create: 2021-03-13 18:35
+ **/
+public class 移除链表元素 {
+
+    @Test
+    public void test(){
+        int[] array = new int[]{1,2,6,3,4,5,6};
+        ListNode listNode = ListNode.get(array);
+        ListNode listNode1 = removeElements(listNode, 6);
+        ListNode.print(listNode1);
+    }
+
+    public ListNode removeElements(ListNode head, int val) {
+        if(head == null) return null;
+        head.next = removeElements(head.next,val);
+
+        if(head.val == val){
+            return head.next;
+        }else{
+            return head;
+        }
+    }
+
+}
